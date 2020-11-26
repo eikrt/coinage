7c7592cb5e2171b0933c0c036d0b4f3ceb2fe198
Eino Korte <einokorte@pop-os.localdomain>
2020-11-26 22:44:37 +0200

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to coinage
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/coinage/core_test.clj
@@ -0,0 +1,7 @@
+(ns coinage.core-test
+  (:require [clojure.test :refer :all]
+            [coinage.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
