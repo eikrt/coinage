917f05518e02fe7059e341cce7971a8ff00f06fd
Eino Korte <e.i.korte@gmail.com>
2020-11-29 00:52:55 +0200

    added things to readme and made program to work great

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Coinage
 
-plotting application to predict when to buy things based on income
+plotting application to predict when sold items used in a business should be bought
+
+## Structure
+
+total number of items and total items lost per action in input_data.csv. estimate.csv holds estimates of items sold per month.
+
--- plot.py
+++ plot.py
@@ -8,42 +8,42 @@ BG_COLOR = 'beige'
 def get_file(single_item_cost):
     x=[]
     y=[]
-    with open('data/finances.csv','r') as csvfile:
+    with open('data/estimate.csv','r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
-            x.append(int(row[0]))
-            y.append(int(row[1]))
+            x.append(float(row[0]))
+            y.append(float(row[1]))
         return x, y
 def get_input():
     x=[]
     y=[]
     with open('data/input_data.csv','r') as csvfile:
         input_data = csv.reader(csvfile, delimiter=',')
+        
         for row in input_data:
-            x.append(int(row[0]))
-            y.append(int(row[1]))
-        return x, y
-def get_cost(single_item_cost, volume):
-    return [single_item_cost * volume]
-    
+            x.append(float(row[0]))
+        return x
 def get_plt():
-    input_x, input_y = get_input();
+    # getting info right
+    input_x = get_input();
+    currently_in_stock = float(input_x[0]);
+    itemcost_per_item = float(input_x[1]);
     stock_count_x = []
     stock_count_y = []
     x, y = get_file(2)
-    for i in x:
-        for j in y:
+    for i in range(len(x)):
+        for j in range(len(y)):
+            currently_in_stock -= y[i] * itemcost_per_item
             stock_count_x.append(i)
-            stock_count_y.append(j)
+            stock_count_y.append(currently_in_stock)
+    # plotting
     plt.rcParams['axes.facecolor'] = BG_COLOR
     fig = plt.figure(0);
     fig.canvas.set_window_title("Coinage")
-    
-    
     plt.plot(x,y, label='volume', color=SOLD_COLOR)
-    plt.plot(stock_count_x,stock_count_y, label='stock for item:', color=ITEM_COLOR)
+    plt.plot(stock_count_x,stock_count_y, label='stock for item', color=ITEM_COLOR)
     plt.xlabel('Month')
-    plt.ylabel('Income €')
+    plt.ylabel('Volume')
     plt.title('')
     plt.legend()
     plt.show()
